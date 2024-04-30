6d70f232baa03a5c171f5464d1477a967ecb17a0
Nikhl <nikhil.singh@xenonstack.com>
2024-04-30 09:53:55 +0530

    front end need some more changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AddBook from "./pages/AddBook";
-import RemoveBook from "./pages/RemoveBook";
-import ListIssue from "./pages/ListIssue";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AddBook />}>
-          <Route index element={<RemoveBook />} />
-          <Route path="listissue" element={<ListIssue />} />
-          {/* <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/pages/AddBook.jsx
+++
@@ -1,8 +0,0 @@
-const AddBook=()=>{
-    return(
-        <>
-        <h1>Hello</h1>
-        </>
-    );
-}
-export default AddBook;
